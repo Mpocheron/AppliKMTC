3bb8dd34466431aae02b3d47ebb2da2de7071f0b
Carole-B <jogabonito@hotmail.fr>
2024-04-09 13:53:51 +0200

    base + home

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@ import 'bootstrap';
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
+import './scriptnavbar.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
