f302ba77840efa7965d07e9776a7cc421b6ce43f
Milowwa <145760117+Milowwa@users.noreply.github.com>
2024-04-09 11:45:05 +0200

    ajout homepage user, UserController et formulaire editUser

---
+++ src/Controller/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;  
+use App\Form\EditUserType;
+use App\Form\UserType;  
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * Ce controller nous permet d'éditer le profil de l'utilisateur connecté
+     * 
+     * @param User $user
+     * @param EntityManagerInterface $entityManager
+     * @param UserPasswordHasherInterface $userPasswordHasher
+     * @param Request $request
+     * @return Response
+     */
+
+    #[Route('/user', name: 'home_user')]
+
+    public function homeuser(Request $request): Response
+    {
+        return $this->render('user/homeuser.html.twig');
+    }
+
+
+    #[Route('/user/edit', name: 'app_user')] 
+    
+    public function editUser(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        //Si l'utilisateur n'est pas connecté, il est redirigé vers la page de connexion
+
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+
+        $form = $this->createForm(EditUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $user = $form->getData();
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+                );
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre profil a bien été modifié');
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'editUserForm' => $form->createView(),
+        ]);
+    }
+
+
+}
