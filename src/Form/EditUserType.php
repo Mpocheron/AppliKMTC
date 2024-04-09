ea3315175be1132669b15d8984f7a52c893b7295
Milowwa <145760117+Milowwa@users.noreply.github.com>
2024-04-09 11:56:32 +0200

    suppression uses inutiles

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use App\Entity\User;
-use App\Controller\UserController;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
