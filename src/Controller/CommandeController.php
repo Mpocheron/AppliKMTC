b4b2c1b7fa7b33f58f66881ab00bdcbc9c335a30
Katia GHN <Katia-GHN.k.guerrouahen@gmail.com>
2024-04-12 22:01:07 +0200

    CommandeForm submit en bdd et getflashbag

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Controller;
-
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Form\CommandeFormType;
 use App\Entity\Commande;
 
@@ -17,26 +19,32 @@ class CommandeController extends AbstractController
      * @Route("/", name="app_commande")
      */
     /*
-    #[Route('/commande', name: 'app_commande')]
-    public function index(): Response
-    {
-        return $this->render('commande/index.html.twig', [
-            'controller_name' => 'CommandeController',
-        ]);
-    }*/
+    */
 
-    #[Route('/commande', name: 'app_commande')]
-    public function commande(): Response
+    #[Route('/createCommande', name: 'app_commande')]
+    public function createCommande(Request $request, EntityManagerInterface $entityManager, SessionInterface $session): Response
     {
-
         //Creation d'une nouvelle commande/=instance
         $commande = new Commande();
-
         //Creation du formulaire avec l'instance commande
         $commandeform = $this->createForm(CommandeFormType::class, $commande);
+        $commandeform->handleRequest($request);
 
+        if ($commandeform->isSubmitted() && $commandeform->isValid()) {
+            $commande = $commandeform->getData();
+            $entityManager->persist($commande);
+            $entityManager->flush();  
+
+            //Ajout d'un flash message en cas de commande validée
+            $session->getFlashBag()->add('success', 'La commande a été validée avec succès.');
+        }
+        else {
+        // Ajout d'un message flash en cas d'erreur
+        $session->getFlashBag()->add('error', 'Une erreur est survenue lors de la validation de la commande.');
+        }
 
         return $this->render('commande/index.html.twig', [ 
+            'user' => $this->getUser(),
             'commandeform' => $commandeform->createView(),
         ]);
     }
--- src/Form/CommandeFormType.php
+++ src/Form/CommandeFormType.php
@@ -7,40 +7,40 @@ use App\Entity\Casier;
 use App\Entity\Commande;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CommandeFormType extends AbstractType
-{
+{   //création du formulaire de commande
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('hauteur', options:[ 'label'=>'Hauteur (en cm)'])
-            ->add('largeur', options:[ 'label'=>'Largeur (en cm)'])
-            ->add('longueur', options:[ 'label'=>'Longueur (en cm)'])
-            ->add('poids', options:[ 'label'=>'Poids (en kg)'])
+        $builder // je n'ai pas intégré les adresses expedition (car récupéré avec user en vue) et destination (car relais colis)
+            ->add('hauteur', options:[ 'label'=>'Hauteur (en cm)',
+                                        'required' => true
+            ])
+            ->add('largeur', options:[ 'label'=>'Largeur (en cm)',
+                                        'required' => true
+            ])
+            ->add('longueur', options:[ 'label'=>'Longueur (en cm)',
+                                        'required' => true
+            ])
+            ->add('poids', options:[ 'label'=>'Poids (en kg)',
+                                        'required' => true,
+
+            ])    
             ->add('leUser', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'nom',
                 'label' => 'Client'
             ])
-            ->add('adresseExpedition', EntityType::class, [
-                'class' => Adresse::class,
-                'choice_label' => function ($adresse) {
-                    // Construire l'étiquette de choix personnalisée avec plusieurs colonnes
-                    return $adresse->getNumero() . ' ' . $adresse->getNom() . ', ' . $adresse->getCodePostal() . ' ' . $adresse->getVille();
-                },
-                'label' => 'Adresse de l\'expéditeur'
-            ])
-            ->add('adresseDestination', EntityType::class, [
-                'class' => adresse::class,
-                'choice_label' => function ($adresse) {
-                    // Construire l'étiquette de choix personnalisée avec plusieurs colonnes
-                    return $adresse->getNumero() . ' ' . $adresse->getNom() . ', ' . $adresse->getCodePostal() . ' ' . $adresse->getVille();
-                },
-                'label' => 'Adresse de destination'
-            ])
+            
+
+            // ici mettre le relais
+            //->add('nom', EntityType::class, ['mapped' => false ])
+            
             ->add('adresseFacturation', EntityType::class, [
                 'class' => Adresse::class,
                 'choice_label' => function ($adresse) {
@@ -49,11 +49,8 @@ class CommandeFormType extends AbstractType
                 },
                 'label' => 'Adresse de facturation'
             ])
-            ->add('leCasier', EntityType::class, [
-                'class' => Casier::class,
-                'choice_label' => 'id',
-                'label' => 'Numéro de casier'
-            ]) 
+
+            ->add('COMMANDER', SubmitType::class,['label'=>'ENVOYER'])
         ;
     }
 
