4e3f16ab28d0be354d0d40b5f5908bfe3007506d
Mpocheron <mateo.pocheron@gmail.com>
2024-05-07 11:40:11 +0200

    Gestion de la création de relais pour les admin

--- src/Controller/RelaisController.php
+++ src/Controller/RelaisController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Adresse;
 use App\Entity\Casier;
+use App\Entity\Modele;
 use App\Entity\Relais;
-use App\Form\RelaisType;
-use App\Form\CasierType;
 use App\Form\CreateRelaisType;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,16 +21,31 @@ class RelaisController extends AbstractController
     #[Route('admin/create/relais', name: 'create_relais')]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $formData = [];
+        $adresse = new Adresse();
         $relais = new Relais();
 
-        $form = $this->createForm(RelaisType::class, $relais);
+        $form = $this->createForm(CreateRelaisType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $relais = $form->getData();
+            $adresse->setNumero($form->get('numeroAdresse')->getData());
+            $adresse->setNom($form->get('nomRue')->getData());
+            $adresse->setCodePostal($form->get('codePostal')->getData());
+            $adresse->setVille($form->get('ville')->getData());
 
+            $relais->setNom($form->get('nomRelais')->getData());
+            $relais->setLeAdresse($adresse);
+
+            $nbGrandCasier = $form->get('nombreCasierGrand')->getData();
+            $nbMoyenCasier = $form->get('nombreCasierMoyen')->getData();
+            $nbPetitCasier = $form->get('nombreCasierPetit')->getData();
+
+            $entityManager->persist($adresse);
             $entityManager->persist($relais);
             $entityManager->flush();
+            
+            $this->createCasiers($nbGrandCasier, $nbMoyenCasier, $nbPetitCasier, $relais, $entityManager);
 
             return $this->redirectToRoute('admin');
         }
@@ -36,5 +53,40 @@ class RelaisController extends AbstractController
         return $this->render('relais/create.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    public function createCasiers(int $nbGrandCasier, int $nbMoyenCasier, int $nbPetitCasier, Relais $relais, EntityManagerInterface $entityManager)
+    {
+        $casierCollection = new ArrayCollection();
+        $repository = $entityManager->getRepository(Modele::class);
+
+        $lesModeles = $repository->findAll();
+
+        for ($nbGrandCasier; $nbGrandCasier > 0; $nbGrandCasier--) {
+            $casier = new Casier;
+            $casier->setleModele($lesModeles[0]);
+            $relais->addLesCasier($casier);
+
+            $entityManager->persist($casier);
+            $entityManager->flush();
+        }
+        
+        for ($nbMoyenCasier; $nbMoyenCasier > 0; $nbMoyenCasier--) {
+            $casier = new Casier;
+            $casier->setleModele($lesModeles[1]);
+            $relais->addLesCasier($casier);
+
+            $entityManager->persist($casier);
+            $entityManager->flush();
+        }
+        
+        for ($nbPetitCasier; $nbPetitCasier > 0; $nbPetitCasier--) {
+            $casier = new Casier;
+            $casier->setleModele($lesModeles[2]);
+            $relais->addLesCasier($casier);
+
+            $entityManager->persist($casier);
+            $entityManager->flush();
+        }
     }
 }
